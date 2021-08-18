821d4a47aa41e95316398cb2f9ab722a5efb4c63
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-08-18 16:40:49 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+  
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2021 || PRANAV VERMA</p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
